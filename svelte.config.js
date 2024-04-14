d62d6487ce13b0f89bc445d02b5632f40fccb243
codnomer <demirhanomer11@gmail.com>
2024-04-14 23:36:28 +0300

    First commitss

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import adapter from '@sveltejs/adapter-auto';
-
+const base = '/port_v1';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
@@ -7,6 +7,9 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter()
+	},
+	paths: {
+		base: process.env.NODE_ENV === 'production' ? base : ''
 	}
 };
 
